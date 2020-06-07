678169d72d0721207ed5f64cb1825f0e3c384077
Your Name <1147893200@qq.com>
2020-06-07 14:23:43 +0800

    本地训练

--- recode.md
+++ recode.md
@@ -5,3 +5,12 @@ epoch0 eval avg cer:0.853
 epoch1 eval avg cer:0.790
 epoch6 eval avg cer:0.76
 epoch7 eval avg cer:0.7587591505347183
+
+epoch20
+eval loss: 0.900949239730835
+eval avg cer:0.37047709108989474
+eval avg wer:0.8462259622323776
+epoch21
+eval loss: 0.8629829287528992
+eval avg cer:0.3700133555155141
+eval avg wer:0.8468222520871544
--- train.py
+++ train.py
@@ -10,19 +10,19 @@ from apex import amp
 
 def evalute(model, loader, device):
     model.eval()
+    model.to(device=device)
     total_cer = 0
     total_wer = 0
     total_count = 0
     print("eval...")
     for (i, batch) in tqdm(enumerate(loader)):
-        model.to(device=device)
         input = batch[0]
         percents = batch[2]
         trans = batch[1]
         trans_lengths = batch[3]
         out = model(input.to(device), percents.to(device))
         t_lengths = torch.mul(out.size(1), percents).int()  # 输出实际长度
-        loss = criterion(out.transpose(0, 1).requires_grad_(),
+        loss = criterion(out.transpose(0, 1),
                          trans, t_lengths, trans_lengths)
         trans_pre = decoder.decode(out)  # 预测文本
         ground_trues = []  # 实际文本
@@ -32,10 +32,12 @@ def evalute(model, loader, device):
             trans_list = trans.narrow(0, start, add).numpy().tolist()
             start += add
             ground_trues.append(decoder.decoder_by_number(trans_list))
-        cer_list_pairs = [(trans_pre[0][i][0], ground_trues[i])
+        cer_list_pairs = [(ground_trues[i].replace(' ', ''), trans_pre[0][i][0].replace(' ', ''))
+                          for i in range(len(trans_lengths))]
+        wer_list_pairs = [(ground_trues[i], trans_pre[0][i][0])
                           for i in range(len(trans_lengths))]
         try:
-            wer = metrics.compute_wer_list_pair(cer_list_pairs)
+            wer = metrics.compute_wer_list_pair(wer_list_pairs)
             cer = metrics.calculate_cer_list_pair(cer_list_pairs)
         except ZeroDivisionError:
             print('ZeroDivisionError')
@@ -63,8 +65,8 @@ dev_dataloader = MyAudioLoader(dev_datasets, batch_size=4, drop_last=False)
 train_datasets = MyAudioDataset(train_manifest_path, labels_path)
 train_dataloader = MyAudioLoader(train_datasets, batch_size=8, drop_last=True)
 criterion = nn.CTCLoss(blank=0, reduction="mean")
-# optim = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
-optim = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
+optim = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+# optim = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
 decoder = GreedyDecoder(labels_path)
 if torch.cuda.is_available():
     map_location = 'cuda:0'
@@ -77,18 +79,18 @@ else:
 model.to(device=device)
 opt_level = 'O1'
 model, optim = amp.initialize(model, optim, opt_level=opt_level)
-checkpoint = torch.load('checkpoint/0.pt')
+checkpoint = torch.load('checkpoint/29.pt')
 model.load_state_dict(checkpoint['model'])
 optim.load_state_dict(checkpoint['optimizer'])
 amp.load_state_dict(checkpoint['amp'])
-for epoch in range(150):
+evalute(model, dev_dataloader, device)
+for epoch in range(29, 150):
     # torch.save(model, "checkpoint/"+str(epoch)+".pt")
     total_cer = 0
     total_wer = 0
     total_count = 0
     model.train()
     for (i, batch) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
-        optim.zero_grad()
         input = batch[0]
         percents = batch[2]
         trans = batch[1]
@@ -97,7 +99,7 @@ for epoch in range(150):
         t_lengths = torch.mul(out.size(1), percents).int()  # 输出实际长度
         loss = criterion(out.transpose(0, 1).requires_grad_(),
                          trans, t_lengths, trans_lengths)
-        # loss.backward()
+        optim.zero_grad()
         with amp.scale_loss(loss, optim) as scaled_loss:
             scaled_loss.backward()
         optim.step()
@@ -112,10 +114,12 @@ for epoch in range(150):
             ground_trues.append(decoder.decoder_by_number(trans_list))
         # true = trans.narrow(0,0,trans_lengths[0].int()).int().numpy().tolist()
         # print(ground_trues[0])
-        cer_list_pairs = [(trans_pre[0][i][0], ground_trues[i])
+        cer_list_pairs = [(ground_trues[i].replace(' ', ''), trans_pre[0][i][0].replace(' ', ''))
+                          for i in range(len(trans_lengths))]
+        wer_list_pairs = [(ground_trues[i], trans_pre[0][i][0])
                           for i in range(len(trans_lengths))]
         try:
-            wer = metrics.compute_wer_list_pair(cer_list_pairs)
+            wer = metrics.compute_wer_list_pair(wer_list_pairs)
             cer = metrics.calculate_cer_list_pair(cer_list_pairs)
         except ZeroDivisionError:
             print('ZeroDivisionError')
