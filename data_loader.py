232e25eaba77ec3cf6334d8dbcb97ebee3a9b04f
kouyt5 <1147893200@qq.com>
2020-07-28 19:45:24 +0800

    apc数据集读入

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset, DataLoader
 import json
 from tqdm import tqdm
 import os
+# import h5py
 
 class MyAudioDataset(Dataset):
     def __init__(self, manifest_path, labels_path, max_duration=16,mask=False):
@@ -98,8 +99,8 @@ class MyAudioLoader(DataLoader):
         targets = torch.IntTensor(targets)
         return inputs, targets, input_percentages, target_sizes, paths
 
-class H5Wav2Vec(Dataset):
-    def __init__(self, manifest_path, labels_path, max_duration=16,mask=False):
+class APCDataset(Dataset):
+    def __init__(self, manifest_path, labels_path, h5_file_path=None, max_duration=16,mask=False):
         self.mask = mask
         self.datasets = []
         self.labels = {}
@@ -108,6 +109,8 @@ class H5Wav2Vec(Dataset):
                 data = json.loads(line, encoding='utf-8')
                 if data['duration'] > max_duration:
                     continue
+                audio_name = data['audio_filepath'].split('/')[-1].split('.')[0] + ".pt"
+                data['audio_filepath'] = os.path.join(h5_file_path,audio_name)
                 self.datasets.append(data)
         with open(labels_path, encoding='utf-8') as f:
             idx = [char.replace('\n', '') for char in f.readlines()]
@@ -117,39 +120,36 @@ class H5Wav2Vec(Dataset):
     def __getitem__(self, index):
         data = self.datasets[index]
         text2id = [self.char2index[char] for char in data['text']]
-        return self.parse_audio(data["audio_filepath"],mask=self.mask), text2id, data['audio_filepath']
+        return self.parse_audio(data["audio_filepath"]), text2id, data['audio_filepath']
 
-    def parse_audio(self, audio_path, win_len=0.02,mask=False):
+    def parse_audio(self, audio_path):
         if not os.path.exists(path=audio_path):
-            raise("音频路径不存在 "+ audio_path)
-        y, sr = torchaudio.load(audio_path)
-        n_fft = int(win_len * sr)
-        hop_length = n_fft // 2
-        transformer = torchaudio.transforms.MelSpectrogram(sr, n_fft=n_fft,
-                                                           hop_length=hop_length, n_mels=64)
-        spec = transformer(y)
-        y = torchaudio.transforms.AmplitudeToDB(stype="power")(spec)
+            raise("h5文件路径不存在 "+ audio_path)
+        y = torch.load(audio_path)
+        y = torch.transpose(y,1,2)
+        return y
+        # with h5py.File(file_path,'r') as f:
+        #     data = f['features']
+        #     y = torch.Tensor(data.value)
 
-        # F-T mask
-        audio_f_mask = torchaudio.transforms.FrequencyMasking(freq_mask_param=10)
-        audio_t_mask = torchaudio.transforms.TimeMasking(time_mask_param=30)
-        if mask:
-            y = audio_f_mask(y)
-            y = audio_t_mask(y)
-        # 归一化
-        std, mean = torch.std_mean(y)
-        y = torch.div((y-mean), std)
         return y  # (1,64,T)
+    
+    def __len__(self):
+        return len(self.datasets)
+
 if __name__ == "__main__":
     manifest_path = "./data/dev-clean.json"
     labels_path = "./data/labels.txt"
+    h5_path = "/mnt/volume/workspace/Autoregressive-Predictive-Coding/extra/dev_clean"
     dataset = MyAudioDataset(manifest_path, labels_path)
+    apcdatasets = APCDataset(manifest_path, labels_path,h5_path)
     # datasets测试
     data = dataset.__getitem__(1)
+    data2 = apcdatasets.__getitem__(1)
     txt2id = data[1]
     id2txt = dataset.id2txt(txt2id)
     # dataloader 测试
-    dataloader = MyAudioLoader(dataset,shuffle=True,batch_size=8)
+    dataloader = MyAudioLoader(apcdatasets,shuffle=True,batch_size=8)
     for batch in enumerate(dataloader):
         print("inputs:" + str(batch[1][0].size()))
         print("targets:" + str(batch[1][1].size()))
